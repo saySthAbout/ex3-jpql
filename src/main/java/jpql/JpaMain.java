6a8ee290d28ec3c3a16d1b3e7ba082124e440fe6
saySthAbout <saysthabout@gmail.com>
2022-12-09 19:25:07 +0900

    10 객체지향 쿼리 언어1 - 기본 문법 10-6 서브 쿼리
    서브 쿼리
    ---
    - 나이가 평균보다 많은 회원
    select m from Member m
    where m.age > (select avg(m2.age) from Member m2)
    **참고: main query와 sub query가 전혀 연관이 없는데 이렇게 짜야 성능이 잘 나온다.
    
    - 한 건이라도 주문한 고객
    select m from Member m
    where (select count(o) from Order o where m = o.member) > 0
    
    서브 쿼리 지원 함수
    ---
    - [NOT] EXISTS (subquery): 서브쿼리에 결과가 존재하면 참
    	- {ALL | ANY | SOME} (subquery)
    	- ALL 모두 만족하면 참
    	- ANY, SOME: 같은 의미, 조건을 하나라도 만족하면 참
    - [NOT] IN (subquery): 서브쿼리의 결과 중 하나라도 같은 것이 있으면 참
    
    서브 쿼리 - 예제
    ---
    - 팀A 소속인 회원
    select m from Member m
    where exists (select t from m.team t where t.name = ‘팀A')
    
    - 전체 상품 각각의 재고보다 주문량이 많은 주문들
    select o from Order o
    where o.orderAmount > ALL (select p.stockAmount from Product p)
    
    - 어떤 팀이든 팀에 소속된 회원
    select m from Member m
    where m.team = ANY (select t from Team t)
    
    JPA 서브 쿼리 한계
    ---
    - JPA는 WHERE, HAVING 절에서만 서브 쿼리 사용 가능
    - SELECT 절도 가능(하이버네이트에서 지원)
    - FROM 절의 서브 쿼리는 현재 JPQL에서 불가능
    	- 조인으로 풀 수 있으면 풀어서 해결
    	- 그것도 안 되면 네이티브 쿼리 쓰거나 따로 애플리케이션에서 조립하거나 쿼리 2번 날리거나 한다. (그런데 네이티브를 잘 안 쓰는 이유는 뭐 sql에 뷰가 있거나 가공 더해야 하고 그럴수 있기 때문에)

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,7 +28,7 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m from Member m left join Team t on m.username = t.name";
+            String query = "select mm.age, mm.username from (select m.age, m.username from Member m) as mm";
             List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
 
