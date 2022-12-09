a90b564bc43b46e676cd864e8c6f8b0736a88b7e
saySthAbout <saysthabout@gmail.com>
2022-12-09 19:07:40 +0900

    10 객체지향 쿼리 언어1 - 기본 문법 10-5 조인
    조인
    ---
    - 내부 조인: SELECT m FROM Member m [INNER] JOIN m.team t
    - 외부 조인: SELECT m FROM Member m LEFT [OUTER] JOIN m.team t
    - 세타 조인: select count(m) from Member m, Team t where m.username = t.name (카르테시안 곱)
    
    조인 - ON 절
    ---
    - ON절을 활용한 조인(JPA 2.1부터 지원)
    	- 1. 조인 대상 필터링
    	- 2. 연관관계 없는 엔티티 외부 조인(하이버네이트 5.1부터)
    
    1. 조인 대상 필터링
    ---
    - 예) 회원과 팀을 조인하면서, 팀 이름이 A인 팀만 조인
    ```
    JPQL:
    SELECT m, t FROM Member m LEFT JOIN m.team t on t.name = 'A'
    
    SQL:
    SELECT m.*, t.* FROM Member m LEFT JOIN Team t ON m.TEAM_ID=t.id and t.name='A'
    ```
    
    2. 연관관계 없는 엔티티 외부 조인
    ---
    - 예) 회원의 이름과 팀의 이름이 같은 대상 외부 조인
    ```
    JPQL:
    SELECT m, t FROM
    Member m LEFT JOIN Team t on m.username = t.name
    
    SQL:
    SELECT m.*, t.* FROM
    Member m LEFT JOIN Team t ON m.username = t.name
    ```

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -13,25 +13,27 @@ public class JpaMain {
         tx.begin();
 
         try{
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            for(int i=0; i<100; i++){
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+
+            member.setTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
+
             System.out.println("result = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
-            }
+
             tx.commit();
         }catch(Exception e){
             tx.rollback();
