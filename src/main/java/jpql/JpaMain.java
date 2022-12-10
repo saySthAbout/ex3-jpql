ea4e15eef6d9ae1264ad99f6184ce77f73a39b20
saySthAbout <saysthabout@gmail.com>
2022-12-10 16:04:44 +0900

    11 객체지향 쿼리 언어2 - 중급 문법 11-3 페치 조인 2 - 한계
    페치 조인의 특징과 한계
    ---
    - 페치 조인 대상에는 별칭을 줄 수 없다. (쓰게 되면 where 절에서 또 가공해서 영속성 컨텍스트가 헷갈릴 수가 있는데 데이터 그 경우 정합성을 보장하지 않는다.)
    	- 하이버네이트는 가능, 가급적 사용X
    - 둘 이상의 컬렉션은 페치 조인 할 수 없다.
    - 컬렉션을 페치 조인하면 페이징 API(setFirstResult, setMaxResults)를 사용할 수 없다.
    	- 일대일, 다대일 같은 단일 값 연관 필드들은 페치 조인해도 페이징 가능
    	- 하이버네이트는 경고 로그를 남기고 메모리에서 페이징(매우 위험)
    	- 해결방법
    		- 다대일로 경로를 뒤집어서 해결
    		- Entity class에 @BatchSize 어노테이션 적용해서 in에다가 해당 teamId를 적용해켜서 해결한다.
    			- 혹은 BatchSize를 글로벌 세팅으로 가져간다. (보통 실무에서는 글로벌 세팅으로 깔고 쓴다.)
    		- Dto로 쿼리 직접 짠다. -> new Opertaion같은거 써가지고 Dto로 뽑는다.
    ```
    @BatchSize(size = 100) //보통 1,000 이하의 값 중에서 크게 준다.
    ```
    
    페치 조인의 특징과 한계
    ---
    - 연관된 엔티티들을 SQL 한 번으로 조회 - 성능 최적화
    - 엔티티에 직접 적용하는 글로벌 로딩 전략보다 우선함
    - @OneToMany(fetch = FetchType.LAZY) //글로벌 로딩 전략
    - 실무에서 글로벌 로딩 전략은 모두 지연 로딩
    - 최적화가 필요한 곳은 페치 조인 적용
    
    페치 조인 - 정리
    ---
    - 모든 것을 페치 조인으로 해결할 수 는 없음
    - 페치 조인은 객체 그래프를 유지할 때 사용하면 효과적
    - 여러 테이블을 조인해서 엔티티가 가진 모양이 아닌 전혀 다른 결과를 내야 하면, 페치 조인 보다는 일반 조인을 사용하고 필요 한 데이터들만 조회해서 DTO로 반환하는 것이 효과적

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -40,9 +40,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members";
+            String query = "select t From Team t";
 
             List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
             System.out.println("result = " + result.size());
