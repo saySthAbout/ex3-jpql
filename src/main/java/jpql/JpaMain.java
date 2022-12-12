0778ca25efa802ad638a32fb1f7a7c643ae350d7
saySthAbout <saysthabout@gmail.com>
2022-12-12 14:29:32 +0900

    11 객체지향 쿼리 언어2 - 중급 문법 11-5 엔티티 직접 사용
    엔티티 직접 사용 - 기본 키 값
    ---
    - JPQL에서 엔티티를 직접 사용하면 SQL에서 해당 엔티티의 기 본 키 값을 사용
    - [JPQL]
    select count(m.id) from Member m //엔티티의 아이디를 사용
    select count(m) from Member m //엔티티를 직접 사용
    
    - [SQL](JPQL 둘다 같은 다음 SQL 실행)
    select count(m.id) as cnt from Member m
    
    엔티티 직접 사용 - 기본 키 값
    ---
    엔티티를 파라미터로 전달
    ```
    String jpql = “select m from Member m where m = :member”;
    List resultList = em.createQuery(jpql)
     .setParameter("member", member)
     .getResultList();
    ```
    
    식별자를 직접 전달
    ```
    String jpql = “select m from Member m where m.id = :memberId”;
    List resultList = em.createQuery(jpql)
     .setParameter("memberId", memberId)
     .getResultList();
    ```
    
    실행된 SQL
    ```
    select m.* from Member m where m.id=?
    ```
    
    엔티티 직접 사용 - 외래 키 값
    ---
    
    ```
    Team team = em.find(Team.class, 1L);
    String qlString = “select m from Member m where m.team = :team”;
    List resultList = em.createQuery(qlString)
     .setParameter("team", team)
     .getResultList();
    ```
    
    ```
    String qlString = “select m from Member m where m.team.id = :teamId”;
    List resultList = em.createQuery(qlString)
     .setParameter("teamId", teamId)
     .getResultList();
    ```
    
    실행된 SQL
    ```
    select m.* from Member m where m.team_id=?
    ```

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -40,22 +40,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t";
+            String query = "select m From Member m where m.team = :team";
 
-            List<Team> result = em.createQuery(query, Team.class)
-                    .setFirstResult(0)
-                    .setMaxResults(2)
+            List<Member> members = em.createQuery(query, Member.class)
+                    .setParameter("team", teamA)
                     .getResultList();
 
-            System.out.println("result = " + result.size());
+            for (Member member : members) {
+                System.out.println("member = " + member);
+            }
 
-            for (Team team : result) {
-                System.out.println("team = " + team.getName() + "|members=" + team.getMembers().size());
-                for(Member member : team.getMembers()){
-                    System.out.println("-> member = " + member);
-                }
-            } 
-            
             tx.commit();
         }catch(Exception e){
             tx.rollback();
